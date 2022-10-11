358a69a21760a5239b2183830ebd3d50eff68661
Alexandre Gueths <agueths@gmail.com>
2022-10-11 15:46:45 -0300

    complete Comments tests

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -12,116 +12,129 @@ require 'rails_helper'
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/comments", type: :request do
+RSpec.describe '/comments', type: :request do
+  article = if Article.exists?(id: 1)
+              Article.find_by(id: 1)
+            else
+              Article.create(
+                {
+                  title: 'TDD',
+                  body: 'Quando os testes vem antes'
+                }
+              )
+            end
   # This should return the minimal set of attributes required to create a valid
   # Comment. As you add validations to Comment, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) do
+    { article_id: article.id, name: 'Alexandre', comment: 'E vem mesmo' }
+  end
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) do
+    { article_id: nil, name: 'Alexandre', comment: 'E vem mesmo' }
+  end
 
   # This should return the minimal set of values that should be in the headers
   # in order to pass any filters (e.g. authentication) defined in
   # CommentsController, or in your router and rack
   # middleware. Be sure to keep this updated too.
-  let(:valid_headers) {
+  let(:valid_headers) do
     {}
-  }
+  end
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       Comment.create! valid_attributes
       get comments_url, headers: valid_headers, as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
+  describe 'GET /show' do
+    it 'renders a successful response' do
       comment = Comment.create! valid_attributes
       get comment_url(comment), as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Comment" do
-        expect {
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Comment' do
+        expect do
           post comments_url,
                params: { comment: valid_attributes }, headers: valid_headers, as: :json
-        }.to change(Comment, :count).by(1)
+        end.to change(Comment, :count).by(1)
       end
 
-      it "renders a JSON response with the new comment" do
+      it 'renders a JSON response with the new comment' do
         post comments_url,
              params: { comment: valid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:created)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Comment" do
-        expect {
+    context 'with invalid parameters' do
+      it 'does not create a new Comment' do
+        expect do
           post comments_url,
                params: { comment: invalid_attributes }, as: :json
-        }.to change(Comment, :count).by(0)
+        end.to change(Comment, :count).by(0)
       end
 
-      it "renders a JSON response with errors for the new comment" do
+      it 'renders a JSON response with errors for the new comment' do
         post comments_url,
              params: { comment: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
   end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        { name: 'Teste', comment: 'Comentário alterado' }
+      end
 
-      it "updates the requested comment" do
+      it 'updates the requested comment' do
         comment = Comment.create! valid_attributes
         patch comment_url(comment),
               params: { comment: new_attributes }, headers: valid_headers, as: :json
         comment.reload
-        skip("Add assertions for updated state")
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to match(a_string_including('application/json'))
+        expect(response.body).to include new_attributes[:name]
+        expect(response.body).to include new_attributes[:comment]
       end
 
-      it "renders a JSON response with the comment" do
+      it 'renders a JSON response with the comment' do
         comment = Comment.create! valid_attributes
         patch comment_url(comment),
               params: { comment: new_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:ok)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "renders a JSON response with errors for the comment" do
+    context 'with invalid parameters' do
+      it 'renders a JSON response with errors for the comment' do
         comment = Comment.create! valid_attributes
         patch comment_url(comment),
               params: { comment: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested comment" do
+  describe 'DELETE /destroy' do
+    it 'destroys the requested comment' do
       comment = Comment.create! valid_attributes
-      expect {
+      expect do
         delete comment_url(comment), headers: valid_headers, as: :json
-      }.to change(Comment, :count).by(-1)
+      end.to change(Comment, :count).by(-1)
     end
   end
 end
