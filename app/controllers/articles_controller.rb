e3847fcedb10fdfb8c840415e1ae867bba5c4e06
Alexandre Gueths <agueths@gmail.com>
2022-10-11 01:20:53 -0300

    create and show Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,22 @@ class ArticlesController < ApplicationController
     @articles = Article.all
     render json: @articles, status: 200
   end
+
+  def show
+    @article = Article.find(params[:id])
+    render json: @article, status: 200
+  rescue ActiveRecord::RecordNotFound
+    render json: {}, status: 404
+  end
+
+  def create
+    @article = Article.create(article_params)
+    render json: @article, status: 201
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'ArticlesController', type: :request do
   context 'index' do
     it 'should return status ok' do
       # request
-      get articles_index_path
+      get articles_path
 
       # what expect
       expect(response).to have_http_status(:success)
@@ -21,12 +21,12 @@ RSpec.describe 'ArticlesController', type: :request do
       other_article = Article.create(
         {
           title: 'Tenha sucesso profissional',
-          body: 'Faça Análise de Sistemas na Guairacá'
+          body: 'Faça Engenharia de Software na Campo Real'
         }
       )
 
       # request
-      get articles_index_path
+      get articles_path
 
       # what expect
       expect(response).to have_http_status(:success)
@@ -34,6 +34,42 @@ RSpec.describe 'ArticlesController', type: :request do
       expect(response.body).to include article.body
       expect(response.body).to include other_article.title
       expect(response.body).to include other_article.body
+    end
+  end
+
+  context 'show' do
+    it 'should return an article' do
+      article = Article.create(
+        {
+          title: 'Estudando Ruby',
+          body: 'Faça meu workshop e aprenda a começar'
+        }
+      )
+
+      get article_path(article)
+
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include article.title
+      expect(response.body).to include article.body
+    end
+
+    it 'should return not found if article does not exist' do
+      get article_path(id: 999)
+
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  context 'create' do
+    it 'should create a new article' do
+      article = { article: { title: 'TDD', body: 'O melhor jeito de programar sem ter erros' } }
+
+      post articles_path, params: article
+
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('TDD')
+      expect(response.body).to include('O melhor jeito de programar sem ter erros')
+      expect(Article.last.title).to eq('TDD')
     end
   end
 end
